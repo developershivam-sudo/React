5eddb873416f30622bf90778b3d5dd4c96dc2191
SHIVAM <shivambtechcse2023@gmail.com>
2025-04-07 21:10:27 +0530

    api handled

--- notes.md
+++ notes.md
@@ -36,3 +36,67 @@ function App (){
 
 export default App
 ```
+
+```React
+import Card from "./components/Card"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+const users = [
+  {
+    "name": "Alice Johnson",
+    "age": 28,
+    "city": "New York",
+    "profession": "Software Engineer",
+    "profilePhoto": "https://example.com/photos/alice.jpg"
+  },
+  {
+    "name": "Ravi Kumar",
+    "age": 32,
+    "city": "Bangalore",
+    "profession": "Data Analyst",
+    "profilePhoto": "https://example.com/photos/ravi.jpg"
+  },
+  {
+    "name": "Emily Chen",
+    "age": 24,
+    "city": "San Francisco",
+    "profession": "UX Designer",
+    "profilePhoto": "https://example.com/photos/emily.jpg"
+  },
+  {
+    "name": "El-Sayed",
+    "age": 30,
+    "city": "Cairo",
+    "profession": "Cybersecurity Specialist",
+    "profilePhoto": "https://example.com/photos/mohamed.jpg"
+  },
+  {
+    "name": "Sofia Martinez",
+    "age": 27,
+    "city": "Madrid",
+    "profession": "Marketing Manager",
+    "profilePhoto": "https://example.com/photos/sofia.jpg"
+  }
+]
+
+function App (){
+  
+  return (
+    <>
+      <Navbar />
+      <div className="p-10 flex gap-4 text-white">
+          {users.map( (element, idx) => {
+            return <div key = {idx}>
+              <Card name={element.name} age={element.age} city={element.city} profession={element.profession} profilePhoto={element.profilePhoto}/>
+            </div>
+          })}
+        </div>
+        
+      <Footer />
+    </>
+  )
+}
+
+export default App
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,31 @@
-import Card from "./components/Card"
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
+import axios from 'axios';
+import React, { useState } from 'react';
 
-const users = [
-  {
-    "name": "Alice Johnson",
-    "age": 28,
-    "city": "New York",
-    "profession": "Software Engineer",
-    "profilePhoto": "https://example.com/photos/alice.jpg"
-  },
-  {
-    "name": "Ravi Kumar",
-    "age": 32,
-    "city": "Bangalore",
-    "profession": "Data Analyst",
-    "profilePhoto": "https://example.com/photos/ravi.jpg"
-  },
-  {
-    "name": "Emily Chen",
-    "age": 24,
-    "city": "San Francisco",
-    "profession": "UX Designer",
-    "profilePhoto": "https://example.com/photos/emily.jpg"
-  },
-  {
-    "name": "El-Sayed",
-    "age": 30,
-    "city": "Cairo",
-    "profession": "Cybersecurity Specialist",
-    "profilePhoto": "https://example.com/photos/mohamed.jpg"
-  },
-  {
-    "name": "Sofia Martinez",
-    "age": 27,
-    "city": "Madrid",
-    "profession": "Marketing Manager",
-    "profilePhoto": "https://example.com/photos/sofia.jpg"
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=20')
+    console.log(response.data);
+    
+    setData(response.data);
   }
-]
 
-function App (){
-  
   return (
-    <>
-      <Navbar />
-      <div className="p-10 flex gap-4 text-white">
-          {users.map( (element, idx) => {
-            return <div key = {idx}>
-              <Card name={element.name} age={element.age} city={element.city} profession={element.profession} profilePhoto={element.profilePhoto}/>
+    <div className='p-10'>
+      <button onClick={getData} className='bg-teal-700 text-white font-semibold text-2xt px-6 py-3 rounded active:scale-90'>Get Data</button>
+      <div className='p-5 mt-5 bg-gray-950 text-white'>
+        {
+          data.map( (element, idx) => {
+            return <div key = {idx} className='text-black flex items-center justify-between w-full px-7 py-6 rounded mb-3'>
+              <img className='h-40' src={element.download_url} alt="" />
+              <h1 className='text-white px-4'>{element.author}</h1>
             </div>
-          })}
-        </div>
-        
-      <Footer />
-    </>
-  )
-}
+          })
+        }
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
